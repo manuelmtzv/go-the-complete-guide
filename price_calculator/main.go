68a0dfdb6bb6c66069c99b74ea56f9cc936693c4
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-06-25 19:08:03 -0600

    feat: advance price calculator project

--- price_calculator/main.go
+++ price_calculator/main.go
@@ -1,7 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	prices := []float64{100, 200, 300}
 	taxRates := []float64{0.1, 0.2, 0.3}
 
+	result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+		for i, price := range prices {
+			taxIncludedPrices[i] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+
+	fmt.Println(result)
 }
